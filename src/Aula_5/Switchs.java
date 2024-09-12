6fe78fcca86919f6baf84d7a0831e48678fd7b57
Arthur <arthurhenrrique1705@gmail.com>
2024-09-12 16:03:59 -0300

    Aula sobre Switch case

--- src/Aula_5/Switchs.java
+++ src/Aula_5/Switchs.java
@@ -1,4 +1,26 @@
 package Aula_5;
 
+import java.util.Scanner;
+
 public class Switchs {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        String cor_sem = teclado.nextLine();
+
+        switch (cor_sem){
+            case "Vermelho":
+                System.out.println("Pare");
+                break;
+
+            case "Amarelo":
+                System.out.println("Desacelere");
+                break;
+
+            case "Verde":
+                System.out.println("Continue");
+                break;
+
+        }
+    }
 }
