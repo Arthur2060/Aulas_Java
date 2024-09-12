3d61e3b0c95c921dbac0d4c27b6f4f910bbc9640
Arthur <arthurhenrrique1705@gmail.com>
2024-09-12 20:34:10 -0300

    a

---
+++ src/Aula_5/Calculo_de_comissao.java
@@ -0,0 +1,11 @@
+package Aula_5;
+
+import java.util.Scanner;
+
+public class Calculo_de_comissao {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        
+    }
+}
