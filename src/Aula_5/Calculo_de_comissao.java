86c45d1b9428190267842e63f2cc02cb75e865e6
ogmatos <pedraao107@gmail.com>
2024-09-13 16:07:12 -0300

    a

--- src/Aula_5/Calculo_de_comissao.java
+++ src/Aula_5/Calculo_de_comissao.java
@@ -6,6 +6,20 @@ public class Calculo_de_comissao {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        
+        System.out.println("Bem-vindo ao sistema de calculo de comissões, digite suas vendas: ");
+        float vendas = teclado.nextFloat();
+        System.out.println("R$");
+
+        float comissao = 0;
+        if (vendas <= 1000){
+            comissao = 5 * (vendas / 100);
+            System.out.printf("Ok, sua comissão será de %.2fR$", comissao);
+        }else if (vendas > 1000 || vendas <= 5000){
+            comissao = 10 * (vendas / 100);
+            System.out.printf("Muito bem, sua comissão será de %.2fR$", comissao);
+        }else if (vendas > 5000){
+            comissao = 15 * (vendas / 100);
+            System.out.printf("Perfeito, sua comissão será de %.2fR$", comissao);
+        }
     }
 }
