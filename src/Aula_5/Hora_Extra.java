92613d2403d3351588ef510cb1fb3b82fc423bdf
Arthur <arthurhenrrique1705@gmail.com>
2024-09-14 22:24:48 -0300

    Refinando

--- src/Aula_5/Hora_Extra.java
+++ src/Aula_5/Hora_Extra.java
@@ -11,18 +11,28 @@ public class Hora_Extra {
                 "total de horas semanais do trabalhador: ");
         int horas = teclado.nextInt();
 
+        //Cria uma variavel para armazenar as horas extras
         int horasex = horas - 40;
+
+        //Inicia a variavel que armazenara o bonus
         float bonus;
 
+        //Se houverem horas extras
         if (horasex > 0){
-        System.out.print("Qual o salário regular por hora?: ");
 
-        float sal = teclado.nextFloat();
+            System.out.print("Qual o salário regular por hora?: ");
+            float sal = teclado.nextFloat();
+
+            //Cálcula o valor do bonus
             bonus = horasex * (sal + 5 * (sal / 100));
+
+            //imprime o resultado
             System.out.printf("Ok, foram %d horas trabalhadas a mais, " +
-                            "com salário de %.2fR$ a hora, da um bonus total de %.2fR$",
-                    horasex, sal, bonus);
-        } else if (horasex <= 0) {
+            "com salário de %.2fR$ a hora, da um bonus " +
+            "total de %.2fR$", horasex, sal, bonus);
+        }
+        //Caso não tenham horas extras
+        else{
             System.out.println("Ótimo, nenhuma hora a mais para ser " +
                     "contabilizada");
         }
