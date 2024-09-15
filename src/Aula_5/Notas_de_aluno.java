af0c2e1a4969f9d8bcf08704c3cb231d862149ac
Arthur <arthurhenrrique1705@gmail.com>
2024-09-14 22:31:50 -0300

    Fácil

---
+++ src/Aula_5/Notas_de_aluno.java
@@ -0,0 +1,26 @@
+package Aula_5;
+
+import java.util.Scanner;
+
+public class Notas_de_aluno {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Bem-vindo, este programa " +
+                "dirá se um aluno está aprovado, reprovado " +
+                "ou em recuperação");
+
+        System.out.print("Digite a nota: ");
+        byte nota = teclado.nextByte();
+
+        if (nota > 7){
+            System.out.println("Aluno aprovado");
+        }
+        else if(nota <= 7 && nota >= 5){
+            System.out.println("Passou para a recuperação");
+        }
+        else{
+            System.out.println("Aluno reprovado");
+        }
+    }
+}
