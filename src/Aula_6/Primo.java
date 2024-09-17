17ddef937a8f6666410776bc5dddedb470b997de
Arthur <arthurhenrrique1705@gmail.com>
2024-09-17 14:36:32 -0300

    Primo

---
+++ src/Aula_6/Primo.java
@@ -0,0 +1,26 @@
+package Aula_6;
+
+import java.util.Scanner;
+
+public class Primo {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+
+        int n = teclado.nextInt();
+        int c;
+        int divisivel = 0;
+
+        for (c = n; c > 1; c--){
+            if (n % c == 0){
+                divisivel++;
+            }
+        }
+        if (divisivel > 1){
+            System.out.println("Este numero não é primo.");
+        }else{
+            System.out.println("Este numero é primo.");
+        }
+    }
+}
