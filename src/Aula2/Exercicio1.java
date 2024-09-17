959876fed413b9ba11837d9b4af8bfdadfe671b5
Arthur <arthurhenrrique1705@gmail.com>
2024-09-17 17:15:55 -0300

    WIP

---
+++ src/Aula2/Exercicio1.java
@@ -0,0 +1,22 @@
+package Aula2;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite sua idade: ");
+        int idade = teclado.nextInt();
+
+        System.out.print("Digite sua altura: ");
+        float altura = teclado.nextFloat();
+
+        System.out.print("Digite seu peso: ");
+        double peso = teclado.nextDouble();
+
+        System.out.println("idade: " + idade);
+        System.out.println("altura: " + altura);
+        System.out.println("peso: " + peso);
+    }
+}
