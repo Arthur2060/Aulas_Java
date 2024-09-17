fdbb9ad5dadefeb0bf671185919c61507bd7aedc
Arthur <arthurhenrrique1705@gmail.com>
2024-09-17 14:26:50 -0300

    Fatorial

---
+++ src/Aula_6/Calculo_de_Fatorial.java
@@ -0,0 +1,22 @@
+package Aula_6;
+
+import java.util.Scanner;
+
+public class Calculo_de_Fatorial {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite o numero a ser calculado: ");
+
+        int n = teclado.nextInt();
+        int c = n - 1;
+
+        do{
+            n = n * c;
+            c--;
+
+        }while(c >= 2);
+
+        System.out.println(n);
+    }
+}
