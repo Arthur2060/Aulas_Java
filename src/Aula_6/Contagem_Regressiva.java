2f4807f6b98a79a650113a63fc8809f97a30eb66
Arthur <arthurhenrrique1705@gmail.com>
2024-09-17 14:12:42 -0300

    Soma de pares

--- src/Aula_6/Contagem_Regressiva.java
+++ src/Aula_6/Contagem_Regressiva.java
@@ -1,11 +1,7 @@
 package Aula_6;
 
-import java.util.Scanner;
-
 public class Contagem_Regressiva {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-
         int c;
         for(c = 10; c >= 1; c--){
             System.out.println(c);
--- src/Aula_6/Soma_de_Pares.java
+++ src/Aula_6/Soma_de_Pares.java
@@ -1,11 +1,7 @@
 package Aula_6;
 
-import java.util.Scanner;
-
 public class Soma_de_Pares {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-
         int c = 0;
         int n = 0;
 
