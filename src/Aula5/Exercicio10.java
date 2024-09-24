131aa6933b0a41e0971dbbc26ecc67f5af372cb6
Arthur <arthurhenrrique1705@gmail.com>
2024-09-24 17:08:24 -0300

    ok

--- src/Aula5/Exercicio10.java
+++ src/Aula5/Exercicio10.java
@@ -1,4 +1,31 @@
 package Aula5;
 
+import java.util.Scanner;
+
 public class Exercicio10 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int res = 1;
+        int par = 0;
+        int impar = 0;
+
+        do{
+            res = teclado.nextInt();
+            if (res > 100){
+                System.out.println("Numero muito grande");
+                continue;
+            }else if(res % 2 == 0){
+                par = res;
+            }else{
+                impar = res;
+            }
+
+        }while(res > 0);
+
+        System.out.println("Pares = ");
+        System.out.println("Impares = ");
+
+        teclado.close();
+    }
 }
--- src/Aula5/Exercicio6.java
+++ src/Aula5/Exercicio6.java
@@ -6,6 +6,19 @@ public class Exercicio6 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
+        int n1 = 0;
+        int n2 = 1;
+        int c = 5;
+
+
+        while (c > 0){
+            System.out.println(n1);
+            System.out.println(n2);
+            n1 = n1 + n2;
+            n2 = n1 + n2;
+            c--;
+        }
+
         teclado.close();
     }
 }
