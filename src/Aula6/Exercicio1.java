6f5894f3f94cb7226a4d9311100c03e0131a2d26
Arthur <arthurhenrrique1705@gmail.com>
2024-09-30 17:10:49 -0300

    a

--- src/Aula6/Exercicio1.java
+++ src/Aula6/Exercicio1.java
@@ -1,7 +1,25 @@
 package Aula6;
 
+import java.util.Scanner;
+
 public class Exercicio1 {
     public static void main(String[] args) {
-        
+        Scanner teclado = new Scanner(System.in);
+        int[] vetor = new int[5];
+        int resp = 0;
+
+        for (int i = 0; i < 5; i++) {
+            vetor[i] = teclado.nextInt();
+        }
+        for(int numeros : vetor){
+            resp += numeros;
+        }
+
+        System.out.print("O vetor tem esses valores: ");
+        for(int numeros : vetor){
+            System.out.print(numeros + ", ");
+        }
+        System.out.println();
+        System.out.printf("A soma dá = %d", resp);
     }
 }
