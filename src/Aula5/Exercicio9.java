bf0d6f15e69201d69d6cc532c32050f564d4b055
Arthur <arthurhenrrique1705@gmail.com>
2024-09-30 15:04:45 -0300

    a

--- src/Aula5/Exercicio9.java
+++ src/Aula5/Exercicio9.java
@@ -1,16 +1,25 @@
 package Aula5;
 
+import java.util.Random;
 import java.util.Scanner;
+import java.util.random.*;
 
 public class Exercicio9 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int n = 17;
+        Random random = new Random();
+
+        int n = random.nextInt(100);
         int p;
 
         System.out.println("Estou pensando em um numero, tente adivinhar.");
         do {
             p = teclado.nextInt();
+            if(p > n){
+                System.out.println("Um pouco menos...");
+            }else{
+                System.out.println("Um pouco mais...");
+            }
         }while(p != n);
         System.out.println("Acertou!");
     }
