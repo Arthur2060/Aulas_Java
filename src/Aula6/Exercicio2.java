9a35bfb2e3c5fa84819142806d57ee8d0a1d8638
Arthur <arthurhenrrique1705@gmail.com>
2024-10-01 13:21:08 -0300

    Exercicio2

---
+++ src/Aula6/Exercicio2.java
@@ -0,0 +1,28 @@
+package Aula6;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String[] letras = {"A", "B", "C", "D", "E", "F"};
+
+        System.out.print("Digite uma letra: ");
+        String target = teclado.nextLine();
+        boolean valido = true;
+
+        for(int contador = 0; contador < letras.length; contador++){
+            if (letras[contador].equals(target)){
+                valido = true;
+                break;
+            }else{
+                valido = false;
+            }
+        }
+        if (valido){
+            System.out.println("Essa letra está no vetor.");
+        }else{
+            System.out.println("Essa letra não está no vetor.");
+        }
+    }
+}
