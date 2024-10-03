28be1a2a0be0128be513d44061cfc2df6b2fabd0
Arthur <arthurhenrrique1705@gmail.com>
2024-10-03 16:31:52 -0300

    Exercicio3

--- src/Aula7/Exercicio3.java
+++ src/Aula7/Exercicio3.java
@@ -1,4 +1,19 @@
 package Aula7;
 
+import java.util.Scanner;
+
 public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite o numero: ");
+        int n = teclado.nextInt();
+
+        System.out.printf("O dobro de %d é %d", n, dobrar(n));
+
+        teclado.close();
+    }
+    static int dobrar(int n){
+        return n*2;
+    }
 }
