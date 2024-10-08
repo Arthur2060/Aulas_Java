7e812ce6e26f26a33bf084280619c795abedfdb7
Arthur <arthurhenrrique1705@gmail.com>
2024-10-08 14:15:15 -0300

    Trabalhando no desafio

--- src/Aula7/Desafio.java
+++ src/Aula7/Desafio.java
@@ -5,58 +5,48 @@ import java.util.Scanner;
 public class Desafio {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        String[][] cadastros = new String[10][4];   //Cria uma matriz 10 x 4 para agir como banco de dados;
-        String[] colunas = {"ID", "Nome", "Email", "Telefone"}; //Cria um vetor com os nomes pré prontos de cada seção;
+        String[][] cadastros = new String[10][4];   /*Cria uma matriz 10 x 4 para agir como banco de dados*/
+        String[] colunas = {"ID", "Nome", "Email", "Telefone"}; /*Cria um vetor com os nomes pré prontos de cada seção*/
 
-        //Adiciona os nomes na primeira linha;
+        /*Adiciona os nomes na primeira linha*/
         for (int i = 0; i < cadastros[0].length; i++) {
             cadastros[0][i] = colunas[i];
         }
 
         byte resposta = 1;
 
-        //Para se encaixar nas opções a resposta deve ser um numero entre 1 e 4, 0 ou menor encerrará o programa, maior que
-        //4 exibirá uma mensagem de erro;
+        /*Para se encaixar nas opções a resposta deve ser um numero entre 1 e 4, 0 ou menor encerrará o programa, maior que 4 exibirá uma mensagem de erro*/
         while (resposta > 0 && resposta <= 4) {
-            resposta = escolhaDeOpcao(cadastros, colunas);
+            System.out.println("Bem-vindo ao meu sistema de cadastros, o que quer fazer? " +
+                    "(No momento só posso cadastrar até 10 usuários), para encerrar, digite zero ou menor.");
+            System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            System.out.println("X                   Cadastrar[1]                        X");
+            System.out.println("X                   Exibir cadastros[2]                 X");
+            System.out.println("X                   Atualizar usuário[3]                X");
+            System.out.println("X                   Deletar usuário[4]                  X");
+            System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+            resposta = teclado.nextByte();
+
+            switch (resposta) {
+                case 1:
+                    cadastrar(cadastros, colunas);
+                    break;
+                case 2:
+                    exibirCadastros(cadastros);
+                    break;
+                case 3:
+                    atualizarUsuario(cadastros, colunas);
+                    break;
+                case 4:
+                    deletarUsuario(cadastros);
+                    break;
+                default:
+                    System.out.println("Numero invalido");
+            }
         }
 
         teclado.close();
-    }
-
-    static byte escolhaDeOpcao(String[][] cadastros, String[] colunas){
-        Scanner teclado = new Scanner(System.in);
-
-        System.out.println("Bem-vindo ao meu sistema de cadastros, o que quer fazer? " +
-                "(No momento só posso cadastrar até 10 usuários), para encerrar, digite zero ou menor.");
-        System.out.println("X                   Cadastrar[1]                        X");
-        System.out.println("X                   Exibir cadastros[2]                 X");
-        System.out.println("X                   Atualizar usuário[3]                X");
-        System.out.println("X                   Deletar usuário[4]                  X");
-
-        byte resposta = teclado.nextByte();
-
-        switch (resposta) {
-            case 1:
-                cadastrar(cadastros, colunas);
-                break;
-
-            case 2:
-                exibirCadastros(cadastros);
-                break;
-
-            case 3:
-                atualizarUsuario(cadastros, colunas);
-                break;
-
-            case 4:
-                deletarUsuario(cadastros);
-                break;
-
-            default:
-                System.out.println("Numero invalido");
-        }
-        return resposta;
     }
 
     static int localizarNull(String[][] matriz) {
@@ -89,16 +79,11 @@ public class Desafio {
 
     static void cadastrar(String[][] matriz, String[] colunas){
         Scanner teclado = new Scanner(System.in);
-        String[] usuario = new String[4];
         int linha = localizarNull(matriz);
 
-        for (int i = 0; i < usuario.length; i++) {
-            System.out.print("Digite o " + colunas[i] + ": ");
-            usuario[i] = teclado.nextLine();
-        }
-
-        for(int i = 0; i < matriz[linha].length; i++) {
-            matriz[linha][i] = usuario[i];
+        for (int i = 0; i < matriz[linha].length; i++) {
+            System.out.print(colunas[i] + ": ");
+            matriz[linha][i] = teclado.nextLine();
         }
     }
 
--- src/Aula8/Exercicio1.java
+++ src/Aula8/Exercicio1.java
@@ -1,7 +1,16 @@
 package Aula8;
 
+import java.io.File;
+
 public class Exercicio1 {
     public static void main(String[] args) {
+        File desktop = new File("C:/Users/Aluno/Desktop");
 
+        if (desktop.exists()){
+            System.out.println("Ok");
+        }
+        else{
+            System.out.println("Error");
+        }
     }
 }
